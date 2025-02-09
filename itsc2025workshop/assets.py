98e1ade8666792719bafc765d09ff1358da5004d
spaultify <appauldev+git@gmail.com>
2025-02-09 14:20:45 +0800

    Add initial dataset for the activity

--- itsc2025workshop/assets.py
+++ itsc2025workshop/assets.py
@@ -1,4 +1,5 @@
 from typing import Any
+import random
 import dagster as dg
 import pandas as pd
 import os
@@ -18,6 +19,10 @@ SUPERSTORE_ACTIVITY_DATASET = os.path.join(
 
 def col_pair_generator(col_name: str, row_ids: list[Any]):
     return {"col_name": col_name, "row_ids": row_ids}
+
+
+def scramble_case(text):
+    return "".join(random.choice([char.upper(), char.lower()]) for char in text)
 
 
 @dg.asset(group_name="ingestion")
@@ -41,7 +46,7 @@ def superstore_raw() -> dg.MaterializeResult:
         "Sub-Category": str,
         "Product Name": str,
         "Sales": float,
-        "Quantity": float,
+        "Quantity": int,
         "Discount": float,
         "Profit": float,
         "Loss/Profit?": str,
@@ -86,7 +91,7 @@ def superstore_activity_dataset() -> pd.DataFrame:
         "Sub-Category": str,
         "Product Name": str,
         "Sales": float,
-        "Quantity": float,
+        "Quantity": int,
         "Discount": float,
         "Profit": float,
         "Loss/Profit?": str,
@@ -108,91 +113,108 @@ def superstore_activity_dataset() -> pd.DataFrame:
     missing_cols = [
         col_pair_generator("Order ID", missing_order_ids),
         col_pair_generator("Customer ID", missing_customer_ids),
+        col_pair_generator("Order Date", missing_order_dates),
+        col_pair_generator("Product ID", missing_product_ids),
     ]
 
     for pair in missing_cols:
         df.loc[df["Row ID"].isin(pair["row_ids"]), pair["col_name"]] = None
 
-    # duplicate orders
-    duplicate_orders = [
-        1173,
-        1218,
-        1241,
-        1255,
-        1267,
-        1348,
-        1414,
-        1451,
-        1485,
-        1631,
-        1638,
-        1657,
-        1693,
-        1704,
-        1774,
-        1788,
-        1797,
-        1932,
-        1942,
-        1999,
-    ]
+    # # duplicate orders
+    # duplicate_order_ids = [1173, 1218, 1348, 1414, 1485, 1631, 1638, 1657, 1942, 1999]
 
     # Ship Date is older than Order Date
     older_ship_dates = [3135, 3197, 3246, 3247, 3278, 3388, 3406, 3432, 3453, 3477]
+    for row_id in older_ship_dates:
+        df.loc[df["Row ID"] == row_id, "Ship Date"] = df.loc[
+            df["Row ID"] == row_id, "Ship Date"
+        ].apply(lambda x: x.replace(year=2015))
 
     # Outlier Ship Date
     outlier_ship_dates = [3518, 3529, 3545, 3571, 3624, 3630, 3634, 3672, 3673, 3722]
+    for row_id in outlier_ship_dates:
+        df.loc[df["Row ID"] == row_id, "Ship Date"] = df.loc[
+            df["Row ID"] == row_id, "Ship Date"
+        ].apply(lambda x: x.replace(year=2050))
 
     # Outlier Order Date
     outlier_order_dates = [3798, 3811, 3834, 3876, 3887, 3890, 3896, 3912, 3917, 3950]
+    for row_id in outlier_order_dates:
+        df.loc[df["Row ID"] == row_id, "Order Date"] = df.loc[
+            df["Row ID"] == row_id, "Order Date"
+        ].apply(lambda x: x.replace(year=1998))
 
     # malformed country/region
     malformed_country = [4041, 4119, 4133, 4135, 4209, 4238, 4299, 4327, 4369, 4453]
+    for row_id in malformed_country:
+        df.loc[df["Row ID"] == row_id, "Country/Region"] = df.loc[
+            df["Row ID"] == row_id, "Country/Region"
+        ].apply(scramble_case)
 
     # malformed segment
     malformed_segment = [4504, 4504, 4607, 4628, 4633, 4726, 4818, 4894, 4915, 4943]
+    for row_id in malformed_country:
+        df.loc[df["Row ID"] == row_id, "Segment"] = df.loc[
+            df["Row ID"] == row_id, "Segment"
+        ].apply(scramble_case)
 
     # Ship Mode string is different cases
-    diff_cases_ship_mode = [
-        5002,
-        5007,
-        5012,
-        5019,
-        5025,
-        5067,
-        5077,
-        5077,
-        5079,
-        5102,
-        5129,
-        5130,
-        5140,
-        5145,
-        5150,
-        5151,
-        5157,
-        5159,
-        5165,
-        5190,
-    ]
+    malformed_shipmode = [5002, 5007, 5077, 5079, 5102, 5129, 5130, 5151, 5165, 5190]
+    for row_id in malformed_shipmode:
+        df.loc[df["Row ID"] == row_id, "Ship Mode"] = df.loc[
+            df["Row ID"] == row_id, "Ship Mode"
+        ].apply(scramble_case)
 
     # negative quantity
     negative_quantities = [5200, 5207, 5212, 5258, 5298, 5331, 5369, 5394, 5410, 5471]
+    for row_id in negative_quantities:
+        df.loc[df["Row ID"] == row_id, "Quantity"] = df.loc[
+            df["Row ID"] == row_id, "Quantity"
+        ].apply(lambda x: x * -1)
 
     # sales is not a number
     nan_sales = [5512, 5530, 5756, 5767, 5787, 5807, 5845, 5875, 5928, 5986]
+    for row_id in nan_sales:
+        df.loc[df["Row ID"] == row_id, "Sales"] = random.choice(
+            [None, "Paul", "Ian", "Aldrich", "2025-02-09"]
+        )
 
     # profit is not a number
     nan_profit = [6066, 6078, 6078, 6106, 6161, 6172, 6256, 6266, 6268, 6400]
+    for row_id in nan_profit:
+        df.loc[df["Row ID"] == row_id, "Sales"] = random.choice(
+            [None, "N/A", "Could be blank", "hahaha", "2025-02-09", "No profits???"]
+        )
 
     # quantity is outlier
     outlier_quantity = [6503, 6721, 6736, 6825, 7021, 7043, 7047, 7102, 7195, 7348]
+    for row_id in outlier_quantity:
+        random_multiplier = random.choice([500, 1000, 1500])
+        print(random_multiplier)
+        df.loc[df["Row ID"] == row_id, "Quantity"] = df.loc[
+            df["Row ID"] == row_id, "Quantity"
+        ].apply(lambda x: x * random_multiplier)
+        print(df.loc[df["Row ID"] == row_id, "Quantity"])
 
     # sales is outlier
     outlier_sales = [7514, 7545, 7767, 7922, 8072, 8075, 8187, 8242, 8283, 8389]
+    for row_id in outlier_sales:
+        random_multiplier = random.choice([1000, -1000, 500, -500])
+        print(random_multiplier)
+        df.loc[df["Row ID"] == row_id, "Sales"] = df.loc[
+            df["Row ID"] == row_id, "Sales"
+        ].apply(lambda x: x * random_multiplier)
+        print(df.loc[df["Row ID"] == row_id, "Sales"])
 
     # discount is outlier
     outlier_discount = [8668, 8793, 8824, 8832, 8838, 9025, 9195, 9451, 9464, 9484]
+    for row_id in outlier_discount:
+        random_discount = random.choice([10, 15, -15, -10, 20, -20])
+        print(random_multiplier)
+        df.loc[df["Row ID"] == row_id, "Discount"] = df.loc[
+            df["Row ID"] == row_id, "Discount"
+        ].apply(lambda x: x + random_discount)
+        print(df.loc[df["Row ID"] == row_id, "Discount"])
 
     df.to_csv(SUPERSTORE_ACTIVITY_DATASET, index=False)
 
